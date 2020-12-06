281b0727e367253274f451b88e0fd1c1b29f6b0b
HedonisticOpportunist <fujitahibaraki@gmail.com>
2020-12-06 19:33:18 +0000

    Add files via upload

--- cypress_tests/invalid_login.js
+++ cypress_tests/invalid_login.js
@@ -3,30 +3,21 @@ context('Startup', () => {
     cy.visit('https://hedonisticopportunist.github.io/framework_tests/login.html');
   });
   
-   it('Should fill out the login form and redirect to the dashboard', () => {
+   it('Should not redirect to the dashboard', () => {
 	   
 	   cy.title().should('eq', 'Login 💀')
 	   cy.get('form')
 	   cy.get('input[name="username"]')
-	   .type('user')
+	   .type('rsa')
 	   
 	   cy.get('input[name="password"]')
-	   .type('holden')
+	   .type('rsa')
 	   
 	   cy.get('[name="submit"]').click();
 	   
-	   cy.title()
-	   .should('eq', 'Dashboard 💀')
-	   
+	   cy.title().should('eq', 'Login 💀')
 	   cy.get('h1')
-	   .contains('Muppet Musical :P')
-	   
-	   cy.location()
-	   .should((loc) => {
-		   expect(loc.pathname.toString()).to.contain('/dashboard');
+	   .contains('Login')
 		   
  });
-   
-  });
-  
 });
